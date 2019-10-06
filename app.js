cf106dd0e760ad7d57b3962541d74448d88981c0
Marlon <luciousmc@icloud.com>
2019-10-05 18:35:12 -0700

    class takes in an array to arrange

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
 class TeamMaker {
-    constructor( names ){
+    constructor( names, teams ){
         this.names = ['Brian', 'Luke', 'Malokai', 'Aliyah', 'Ciara', 'Abel', 'Titus', 'Charlene'];
-        
+        this.teams = 3;
+        this.totalPeople = this.names.length;
     }
+    
 }
